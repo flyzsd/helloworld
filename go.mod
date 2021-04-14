78e913a218ee490dd7456f323ed16ff169d0e738
shudongzhang <zhangshudong@gmail.com>
2021-04-14 09:15:37 +0800

    Import calculator package from github

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module helloworld
 
 go 1.16
+
+//require github.com/flyzsd/calculator v0.0.0
+
+//replace github.com/flyzsd/calculator => ../calculator
+
+require github.com/flyzsd/calculator v0.2.0
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/flyzsd/calculator"
+)
 
+/**
+This is an comment
+*/
 func main() {
 	fmt.Println("Hello World!")
+	sum := calculator.Sum(1, 2)
+	fmt.Printf("sum = %d\n", sum)
 }
